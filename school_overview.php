4979b19b4583583f0e434aca4a1916a46aab2b22
Dennou Akuma <dennou3331@outlook.jp>
2021-11-08 05:05:34 +0900

    comp gaiyou

--- school_overview.php
+++ school_overview.php
@@ -1,3 +1,34 @@
+<?php
+include "./admin/temp/root.php";
+
+$db_link = new mysqli($mysql_host_A, $mysql_user_A, $mysql_pass_A,$mysql_db_A);
+if (!$db_link) {
+    die('接続失敗'.mysqli_error());
+}
+
+$sql_select = 'select * from base_info where 1 = authority_int or 2 = authority_int or 3 = authority_int order by authority_int';
+
+$user_id_array = array();
+$user_name_array = array();
+$user_text_array = array();
+$esta_text_array = array();
+
+$result_sql = mysqli_query($db_link,$sql_select);
+
+if(!$result_sql){
+    die('失敗'.mysqli_error());
+}else{
+    while ($row = mysqli_fetch_assoc($result_sql)) {
+        array_push($user_id_array,$row['m_id']);
+        array_push($user_name_array,$row['name_str']);
+        array_push($user_text_array,$row['greeting_text']);
+        array_push($esta_text_array,$row['establishment_text']);
+    }        
+}
+
+
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -20,33 +51,46 @@
         </div>
         <div class="content_main">
             <div class="left_main_content">
-                <h2 id="so_A" class="sub_title_h2_A">学長の言葉</h2>
+                <!--学長-->
+                <h2 id="so_A" class="sub_title_h2_A"><?php echo $user_name_array[0]; ?>学長の言葉</h2>
                 <div class="left_sub_contentA">
-                    <p>
-                        今般のコロナ禍により、多くの人々が生存の危機と生活の困難に直面しています。皆さまに心からお見舞い申し上げ、各関係分野でご努力を続けておられる方々に敬意を表し感謝申し上げます。<br>
-                        ゆっくり大学は、ゆっくり解説動画を通じてオンライン授業の拡充することにより、ステイホームやコロナ自粛における皆さまの新しい生活をより有意義なものとできるよう努めてまいります。<br>
-
-                        当学は、「自ら調べ自ら考える力ある人物の育成」及び「ゆっくり解説の門戸を広げる」との建学の精神のもと、動画投稿者を支援するとともに、よりよい動画を視聴者に提供することで、社会を支え、未来を拓く人材を送り出します。<br>
-                        <br>
-                        学問や研究は、実社会とそこに生きる人々と無縁ではなく、現実事象から課題を発見し、解説動画として視聴者にを還元すべきです。また、解説動画は社会の営みや視聴者の反応との相互作用により発展すべきです。よって当学は常に時代とともにあり、社会の変化に適合するよう進化してまいります。<br>
-                        <br>
-                        かかる社会の変化に適合する解説動画を支援することは、「自ら調べ自ら考える力ある人物の育成」という当学の建学の精神に根ざしています。したがって、当学教員の投稿する解説動画は、視聴者の自ら考える力の素地としての知識を伝達することはもとより、コメントやツイッターでの交流の中で培われるコミュニケーション力や議論する力、そして弛まず学び続ける力の涵養にする一助となるものと確信しております。<br>
-                        <br>
-                        今日の日本社会においては、DX（デジタルトランスフォーメーション）の進展に加え、働き方改革の進展やアフターコロナの新生活様式に思いを致すとき、従来にない大きな変化が起こることが予見できます。そこで当学は視聴者がこの歴史的変化に備え、かつよりよい生活を営めるよう学習環境の充実に向けて前進します。<br>
-                        <br>
-                        我々は解説動画自体は勿論のことコメント欄やツイッターといった豊富なリソースを活用し、視聴者と共に学び、成長することを望んでいます。このように「開かれたゆっくり大学」を目指してまいりますので、視聴者さんも遠慮なくリソースを活用してくださいね。
-                    </p>
+                <?php echo $user_text_array[0];?>
                 </div>
                 <div class="right_sub_contentA">
-                    <img src="./media/img/profile.png">
+                    <?php echo '<img src="./media/img/user_img/'.$user_id_array[0].'/pf_img.png">';?>
                     <h2>氏名</h2>
-                    <h3> ゆっくり法律・司法解説チャンネル</h3>
+                    <h3><?php echo $user_name_array[0]; ?></h3>
                 </div>
                 <hr class="float_clearA">
-                <h2 id="so_B" class="sub_title_h2_A">副学長の言葉</h2>
+
+                <!--副学長-->
+                <h2 id="so_B" class="sub_title_h2_A"><?php echo $user_name_array[1]; ?>副学長の言葉</h2>
+                <div class="left_sub_contentB">
+                    <?php echo '<img src="./media/img/user_img/'.$user_id_array[1].'/pf_img.png">';?>
+                    <h2>氏名</h2>
+                    <h3><?php echo $user_name_array[1]; ?></h3>
+                </div>
+                <div class="right_sub_contentB">
+                <?php echo $user_text_array[1];?>
+                </div>
                 <hr class="float_clearA">
+
+                <!--副学長-->
+                <h2 id="so_B" class="sub_title_h2_A"><?php echo $user_name_array[2]; ?>副学長の言葉</h2>
+                <div class="left_sub_contentA">
+                    <?php echo $user_text_array[2];?>
+                </div>
+                <div class="right_sub_contentA">
+                    <?php echo '<img src="./media/img/user_img/'.$user_id_array[2].'/pf_img.png">';?>
+                    <h2>氏名</h2>
+                    <h3><?php echo $user_name_array[2]; ?></h3>
+                </div>
+                <hr class="float_clearA">
+
                 <h2 id="so_C" class="sub_title_h2_A">設立目的</h2>
+                <?php echo $esta_text_array[0]; ?>
                 <hr class="float_clearA">
+
                 <h2 id="so_D" class="sub_title_h2_A">大学の沿革</h2>
                     <ul id="history_ul">
                         <li><p>2021年9月25日</p><p>Discordサーバー設立</p></li>
@@ -63,10 +107,15 @@
                         <li><p>2021年10月29日</p><p>第一回リモート飲み会を開催</p></li>
                         <li><p>2021年10月29日</p><p>派生Discordサーバー「ゆっくり大学学食」を開設</p></li>
                         <li><p>202X年XX月XX日</p><p>公式サイト「ゆっくり大学」を開設</p></li>
-                        
                     </ul>
                 <hr class="float_clearA">
                 <h2 id="so_E" class="sub_title_h2_A">広報</h2>
+                <iframe width="800" height="450" 
+                src="https://www.youtube.com/embed/KT1xfaQwNf0"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
                 <hr class="float_clearA">
                 <h2 id="so_F" class="sub_title_h2_A">情報の扱い</h2>
                 <hr class="float_clearA">
